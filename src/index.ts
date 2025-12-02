0376c8a06e52a7c737c3389c0e87f7a74a91ceca
sychic <47618543+Sychic@users.noreply.github.com>
2025-12-01 23:38:32 -0500

    feat: initial maven repository handling

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,30 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const getUrlPath = (uri: string) => {
+    const url = new URL(uri)
+    return url.pathname
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const path = getUrlPath(request.url)
+		if (request.method == "GET") {
+			let file = await env.STORAGE_BUCKET.get(path)
+			if (file == null) {
+				return new Response(null, { status: 404 })
+			} else {
+				return new Response(file!.body)
+			}
+		}
+		if (request.method == "PUT") {
+			let file = await env.STORAGE_BUCKET.put(path, request.body)
+			if (file == null) {
+				return new Response(null, { status: 500 })
+			} else {
+				return new Response(null, { status: 201 })
+			}
+		}
+		return new Response(`Unsupported method: ${request.method}`, { status: 400 });
 	},
 } satisfies ExportedHandler<Env>;
